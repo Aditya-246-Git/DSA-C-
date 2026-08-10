92e3296ff8e1acbf3b6e207e340ad8d185135594
Aditya-246-Git <rongalirams242007@gmail.com>
2026-08-10 11:03:06 +0530

    Time: 19 ms (91.24%), Space: 11 MB (66.57%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     bool winnerSquareGame(int n) {
-
         vector<int> dp1(n + 1, false);
-
         for (int i = 1; i <= n; i++) {
-
             for (int j = 1; j * j <= i; j++) {
-
                 int square = j * j;
-
                 if (!dp1[i - square]) {
                     dp1[i] = true;
                     break;
                 }
             }
         }
-
         return dp1[n];
     }
 };
