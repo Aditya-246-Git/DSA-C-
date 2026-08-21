7e16c8679600932571989e0e887a5d2efc3e1fd4
Aditya-246-Git <rongalirams242007@gmail.com>
2026-08-21 20:05:40 +0530

    Time: 0 ms (100%), Space: 10.5 MB (73.68%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    bool f(vector<int>& nums, int k, int mid) {
-        int cnt = 1;
-        int sum = 0;
-        for(int x : nums) {
-            if(sum + x > mid) {
-                cnt++;
-                sum = x;
-            }
-            else {
-                sum += x;
-            }
+    bool f(vector<int>& nums,int n, int k ,int mid){
+        int sum=0;
+        int cnt=1;
+        for(int i=0;i<n;i++){
+           sum+=nums[i];
+           if(sum>mid){
+            cnt++;
+            sum=nums[i];
+           }
         }
-        return cnt <= k;
+        if(cnt<=k) return true;
+        return false;
     }
     int splitArray(vector<int>& nums, int k) {
         int n=nums.size();
@@ -20,7 +19,7 @@ public:
         int high=accumulate(nums.begin(),nums.end(),0);
         while(low<=high){
             int mid=low+((high-low)/2);
-            if(!f(nums,k,mid)) low=mid+1;
+            if(!f(nums,n,k,mid)) low=mid+1;
             else high=mid-1;
         }
         return low;
