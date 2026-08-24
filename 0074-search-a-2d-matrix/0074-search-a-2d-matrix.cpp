f91e005d9b64d499fea1a39f5245d23609a124e9
Aditya-246-Git <rongalirams242007@gmail.com>
2026-08-24 20:24:52 +0530

    Time: 0 ms (100%), Space: 13.4 MB (45.12%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -6,11 +6,9 @@ public:
         int low=0;
         int high=n*m-1;
         while(low<=high){
-            int mid=(low+high)/2;
-            int row = mid / m;
-            int col = mid % m;
-            if(matrix[row][col]==target) return true;
-            else if(matrix[row][col]<target) low=mid+1;
+            int mid=low+((high-low)/2);
+            if(matrix[mid / m][mid % m]==target) return true;
+            else if(matrix[mid / m][mid % m]<target) low=mid+1;
             else high=mid-1;
         }
         return false;
