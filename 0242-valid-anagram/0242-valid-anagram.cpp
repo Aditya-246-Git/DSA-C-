273df77f15303e2b9112fa50cba2c33b578b497f
Aditya-246-Git <rongalirams242007@gmail.com>
2026-08-25 19:33:23 +0530

    Time: 0 ms (100%), Space: 9.6 MB (92.5%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,7 +4,7 @@ public:
         int n=s.size();
         int m=t.size();
         if(n!=m) return false;
-        int freq[26]={0};
+        vector<int> freq(26,0);
         for(int i=0;i<n;i++){
             freq[s[i]-'a']++;
             freq[t[i]-'a']--;
