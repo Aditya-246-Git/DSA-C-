d544c5e92f87c5472fd0639b160ea909f5d30824
Aditya-246-Git <rongalirams242007@gmail.com>
2026-09-02 16:14:21 +0530

    Time: 0 ms (100%), Space: 11.4 MB (15.79%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        
         stringstream ss(s);
         string word;
         vector<string> words;
-
-        // words separate cheyyadam
         while(ss >> word) {
             words.push_back(word);
         }
-
-        // words reverse cheyyadam
         reverse(words.begin(), words.end());
-
-        // answer build cheyyadam
         string ans = "";
-
         for(int i = 0; i < words.size(); i++) {
             if(i > 0) {
                 ans += " ";
@@ -24,7 +16,6 @@ public:
 
             ans += words[i];
         }
-
         return ans;
     }
 };
