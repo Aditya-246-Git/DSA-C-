89b4fd55256a3c7add6f37985e05c40859ed8cf7
Aditya-246-Git <rongalirams242007@gmail.com>
2026-09-08 21:20:51 +0530

    Time: 0 ms (100%), Space: 9.2 MB (5.52%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int countCommas(int n) {
-        return max(n - 999, 0); 
+        string s=to_string(n);
+        int l=s.size();
+        if(l<4) return 0;
+        return n-999;
     }
 };
